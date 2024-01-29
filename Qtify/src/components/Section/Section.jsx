ad03cd7f6ee511af31d9d87c851b840124251907
unknown <gishika364@gmail.com>
2024-01-29 16:58:42 +0530

    Test commit

--- Qtify/src/components/Section/Section.jsx
+++ Qtify/src/components/Section/Section.jsx
@@ -1,75 +1,36 @@
-// import React from 'react';
-// import { useEffect,useState } from 'react';
-// import styles from "./Section.module.css";
-// import { CircularProgress } from '@mui/material';
-// import Card from "../Card/Card";
-// import Carousel from '../Carousel/Carousel';
-
-// const Section = ({data,title,type}) => {
-//     const[carouselToggle,setCarouselToggle] = useState(true);
-    
-//     const handleToggle=()=>{
-//         setCarouselToggle((carouselToggle)=>{
-//             return !carouselToggle;
-//         });
-//     }
-      
-//   return (
-//     <div className={styles.container}>
-//         <div className={styles.header}>
-//             <h3>{title}</h3>
-//             <h4 className={styles.toggleText} onClick={handleToggle}>
-//                 {carouselToggle?"Show All":"Collapse"}
-//             </h4>
-//         </div>
-//                 {(data.length===0) ?(<CircularProgress />):(
-//                     <div className = {styles.cardWrapper}>{!carouselToggle?(<div className ={styles.wrapper}>
-//                         {data.map((topAlbum)=>(
-//   <Card data ={topAlbum} type={type} key={topAlbum.id} />
-// ))}
-//                     </div>):(
-//                         <>
-//                     <Carousel data = {data} renderComponent={(dataForSingleCard)=><Card data = {dataForSingleCard} type = {type} key={dataForSingleCard.id}/>} /> </>)}</div>
-//                 )}
-
-//     </div>
-    
-//   )
-// }
-// {/* <Carousel data = {data} renderComponent={(dataForSingleCard)=><Card data = {dataForSingleCard} type = {type}/>} /> */}
-// export default Section
-
 import React from 'react';
 import { useEffect,useState } from 'react';
 import styles from "./Section.module.css";
 import { CircularProgress } from '@mui/material';
 import Card from "../Card/Card";
-// import Carousel from '../Carousel/Carousel';
+import Carousel from '../Carousel/Carousel';
 
 const Section = ({data,title,type}) => {
     const[carouselToggle,setCarouselToggle] = useState(true);
     
-    // const handleToggle=()=>{
-    //     setCarouselToggle((carouselToggle)=>{
-    //         return !carouselToggle;
-    //     });
-    // }
+    const handleToggle=()=>{
+        setCarouselToggle((carouselToggle)=>{
+            return !carouselToggle;
+        });
+    }
       
   return (
     <div className={styles.container}>
         <div className={styles.header}>
             <h3>{title}</h3>
-            <h4 className={styles.toggleText} >
+            <h4 className={styles.toggleText} onClick={handleToggle}>
                 {carouselToggle?"Show All":"Collapse"}
             </h4>
         </div>
-                {
-                    <div className = {styles.cardWrapper}>{(<div className ={styles.wrapper}>
+                {(data.length===0) ?(<CircularProgress />):(
+                    <div className = {styles.cardWrapper}>{!carouselToggle?(<div className ={styles.wrapper}>
                         {data.map((topAlbum)=>(
   <Card data ={topAlbum} type={type} key={topAlbum.id} />
 ))}
-                    </div>)}</div>
-                }
+                    </div>):(
+                        <>
+                    <Carousel data = {data} renderComponent={(dataForSingleCard)=><Card data = {dataForSingleCard} type = {type} key={dataForSingleCard.id}/>} /> </>)}</div>
+                )}
 
     </div>
     
@@ -77,3 +38,4 @@ const Section = ({data,title,type}) => {
 }
 {/* <Carousel data = {data} renderComponent={(dataForSingleCard)=><Card data = {dataForSingleCard} type = {type}/>} /> */}
 export default Section
+
